e207f8af1895f6efa05c877c43fb9f6dc5ea63d9
JeongEuiJin <hehar1020@gmail.com>
2017-05-30 00:25:58 +0900

    출력할때 수정동적으로 보일수있게 꾸밈

--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -1,6 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.utils import timezone
+
+from .models import Post
+
 
 def post_list(request):
-    # return HttpResponse('<html><body>Post List</body></html>')
-    return render(request, 'blog/post_list.html')
+
+    posts = Post.objects.filter(published_date__lte=timezone.now())
+    context = {
+        'title':'PostList from post_list view',
+        'posts':posts,
+    }
+    return render(request, 'blog/post_list.html',context=context)
